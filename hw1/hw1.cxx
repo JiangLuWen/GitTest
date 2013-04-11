395b2493fcbdd221806ba067b2e9ce8daedd81c6
Ding Xuefeng <xfding@whu.edu.cn>
2013-04-11 15:36:58 +0800

    write main() with nothing and Makefile

---
+++ hw1/hw1.cxx
@@ -0,0 +1,34 @@
+/************************************************
+ * Home work of Ding Xuefeng for 312017Z
+ *         <Toolkit in the experiment simulation 
+ *                    and data analysis>
+ * Homework 1
+ * it should be put here:
+ * /user/comphys13S/HomeWork02/${username}/
+ * 
+ * Description:
+ *   Compare histogram with 1e2, 1e3 and 1e4
+ * entries, while pdf are non-linear shape.
+ * 1) Err bars should be plotted, and show the
+ * difference between different cases.
+ * 2) Print mean, RMS, skewness on the plot and
+ * screen
+ * 3) Fit them
+ * 4) Keep pretty
+ *
+ * Author
+ *  Name: Ding Xuefeng
+ *  email: dingxf@ihep.ac.cn
+ *
+ * History
+ *  2013.04.11 ver 0.0
+ *   Plot two hist, run in cint
+ *
+ ***********************************************/
+#include <iostream>
+
+using namespace std;
+int main(){
+  return 0;
+}
+
